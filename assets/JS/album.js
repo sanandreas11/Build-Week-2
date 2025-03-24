c99d40da30dc9af6b1b72fd55eb3f9890527e90f
sanandreas11 <andreaandonis@outlook.com>
2025-03-24 15:14:58 +0100

    adds function to cycle songs

--- assets/JS/album.js
+++ assets/JS/album.js
@@ -1,6 +1,6 @@
 const URLparameters = new URLSearchParams(location.search)
 const albumId = URLparameters.get("id")
-const albumUrl = `https://striveschool-api.herokuapp.com/api/deezer/album/75621062`
+const albumUrl = `https://striveschool-api.herokuapp.com/api/deezer/album/39949511`
 
 const getAlbumDetails = function () {
   fetch(albumUrl + albumId)
@@ -13,7 +13,7 @@ const getAlbumDetails = function () {
       }
     })
     .then((data) => {
-      console.log("DETTAGLI PRODOTTO", data)
+      console.log("DETTAGLI ALBUM", data)
       const albumCover = document.getElementById("album-cover")
       const artistCover = document.getElementById("artist-img-sm")
       const albumName = document.getElementById("album-name")
@@ -25,30 +25,36 @@ const getAlbumDetails = function () {
       albumName.setAttribute("href", "./artist.html?id=" + data.artist.id)
       artistName.innerText = data.artist.name //funziona
       releaseYear.innerText = data.release_date.slice(0, 4) //funziona
+      console.log(data.tracks)
     })
     .catch((err) => {
       console.log("ERRORE NEL RECUPERO DATI CONCERTO", err)
     })
 }
 // LE CANZONI SI TROVANO DENTRO UN ARRAY DI NOME DATA CHE BISOGNA FAR CICLARE PER DISPORRE LE CANZONI DINAMICAMENTE
-// const getSongsFromAlbum = function () {
-//     fetch(albumUrl + albumId)
-//     .then((response) => {
-//       console.log("response", response)
-//       if (response.ok) {
-//         return response.json()
-//       } else {
-//         throw new Error("Errore nel recupero dei dettagli")
-//       }
-//     })
-//     .then((data) => {
-//       data.forEach(track => {
-//        track.
-//       });
-//     })
-//     .catch((err) => {
-//       console.log("ERRORE NEL RECUPERO DATI CONCERTO", err)
-//     })
-// }
+const getSongsFromAlbum = function () {
+  fetch(albumUrl + albumId)
+    .then((response) => {
+      console.log("response", response)
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("Errore nel recupero dei dettagli")
+      }
+    })
+    .then((data) => {
+      data.forEach((tracks) => {
+        const tracklist = document.getElementById("tracklist")
+        tracklist.innerHTML = `<div>
+         <p id="track" class="text-light ms-2">${tracks.title}</p>
+         <p id="artist" class="text-light ms2">${tracks.artist.name}</p>   
+        </div>`
+      })
+    })
+    .catch((err) => {
+      console.log("ERRORE NEL RECUPERO DATI CONCERTO", err)
+    })
+}
 
 getAlbumDetails()
+getSongsFromAlbum()
